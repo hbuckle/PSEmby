a906b154223a956600b5c74b1c3ec35b8792c287
Henry Buckle <h.buckle@outlook.com>
2018-12-29 14:05:02 +0000

    new functions

---
+++ Shared/Get-MkvToolnixOption.ps1
@@ -0,0 +1,60 @@
+class AudioTrack {
+  AudioTrack() {}
+  [string]$Path
+  [string]$Language
+  [string]$Delay
+}
+
+function Get-MkvToolnixOption {
+  [CmdletBinding()]
+  param (
+    [string]$Output,
+    [string]$Video,
+    [string]$AspectRatio,
+    [string]$FPS,
+    [AudioTrack[]]$Audio,
+    [string]$Chapters
+  )
+  $options = @(
+    "--ui-language",
+    "en",
+    "--output",
+    $Output,
+    "--language",
+    "0:eng",
+    "--aspect-ratio",
+    "0:${AspectRatio}",
+    "--default-duration",
+    "0:${FPS}",
+    "(",
+    $Video,
+    ")"
+  )
+  foreach ($track in $Audio) {
+    $options += @(
+      "--language",
+      "0:$($track.Language)",
+      "--sync",
+      "0:$($track.Delay)",
+      "(",
+      $track.Path,
+      ")"
+    )
+  }
+  if (-not([string]::IsNullOrEmpty($Chapters))) {
+    $options += @(
+      "--chapter-language",
+      "und",
+      "--chapters",
+      $Chapters
+    )
+  }
+  $options += "--track-order"
+  $trackscount = 1 + $Audio.Count
+  $trackorder = @()
+  for ($i = 0; $i -lt $trackscount; $i++) {
+    $trackorder += "${i}:0"
+  }
+  $options += ($trackorder -join ",")
+  return ($options | ConvertTo-Json -Depth 99)
+}
