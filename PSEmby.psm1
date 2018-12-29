d915f1cc263962989ab711e11c24ef2542dbdd29
Henry Buckle <h.buckle@outlook.com>
2018-12-29 14:19:49 +0000

    update manifest

--- PSEmby.psm1
+++ PSEmby.psm1
@@ -4,7 +4,13 @@ if (-not(Test-Path "$PSScriptRoot\paths.json")) {
   throw "paths.json not present"
 }
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-Get-ChildItem $PSScriptRoot -Recurse -Include "*.ps1" | ForEach-Object { . $($_.FullName) }
+Get-ChildItem $PSScriptRoot -Recurse -Include "*.ps1" | ForEach-Object {
+  . $($_.FullName)
+}
 Get-ChildItem "$PSScriptRoot\Classes" -Recurse -Include "*.cs" | ForEach-Object {
   Add-Type -Path $_.FullName
 }
+$functions = Get-ChildItem function:\ | Where-Object { $_.Source -eq "PSEmby" } | ForEach-Object {
+  $_.Name
+}
+Update-Metadata -Path "$PSScriptRoot\PSEmby.psd1" -PropertyName "FunctionsToExport" -Value $functions
