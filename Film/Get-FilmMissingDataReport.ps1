2758557b28408aa03d177fc0c239d31938bd7f33
Henry Buckle <h.buckle@outlook.com>
2019-03-02 14:11:12 +0000

    update data report

--- Film/Get-FilmMissingDataReport.ps1
+++ Film/Get-FilmMissingDataReport.ps1
@@ -7,27 +7,33 @@ function Get-FilmMissingDataReport {
     $current = $_.FullName
     try {
       $props = [ordered]@{
-        Folder    = $_.Name
-        FileName  = ""
-        Extension = ""
-        Poster    = $false
-        Backdrop  = 0
-        Logo      = $false
-        Trailer   = $false
-        NFO       = $false
-        Width     = 0
-        Height    = 0
-        Chapters  = $false
-        Title     = ""
-        SortTitle = ""
-        Plot      = $false
-        Director  = $false
-        Actors    = $false
-        Container = ""
+        Folder           = $_.Name
+        FileName         = ""
+        Extension        = ""
+        Poster           = $false
+        Backdrop         = 0
+        Logo             = $false
+        Trailer          = $false
+        Json             = $false
+        VideoCodec       = ""
+        Width            = 0
+        Height           = 0
+        AudioTracks      = 0
+        AudioCodecs      = @()
+        InternalChapters = $false
+        ExternalChapters = $false
+        Title            = ""
+        SortTitle        = ""
+        Plot             = $false
+        Director         = $false
+        Actors           = $false
+        Rating           = ""
+        Container        = ""
       }
       $filmFile = Get-Item "$($_.FullName)\*.mkv"
       $fileInfo = Get-VideoInfo -Path $filmFile.FullName
-      $videoStream = $fileInfo.streams | Where-Object { $_.codec_type -like "video" } | Select-Object -First 1
+      $videoStream = $fileInfo["streams"] | Where-Object { $_["codec_type"] -eq "video" } | Select-Object -First 1
+      $audioStreams = @($fileInfo["streams"] | Where-Object { $_["codec_type"] -eq "audio" })
 
       $props.FileName = $filmFile.BaseName
       $props.Extension = $filmFile.Extension
@@ -35,21 +41,28 @@ function Get-FilmMissingDataReport {
       $props.Backdrop = @(Get-ChildItem $_.FullName -Filter "backdrop*").Count
       $props.Logo = (Test-Path "$($_.FullName)\logo.png")
       $props.Trailer = (Test-Path "$($_.FullName)\Trailers\*.mkv")
-      $props.NFO = (Test-Path "$($_.FullName)\*.nfo")
-      $props.Height = $videoStream.height
-      $props.Width = $videoStream.width
-      $props.Chapters = ($fileInfo.Chapters.Count -gt 0)
-      if ($props.NFO) {
-        $nfoPath = (Resolve-Path "$($_.FullName)\*.nfo").ProviderPath
-        $movie = [embymetadata.movie]::Load($nfoPath)
-        $props.Title = $movie.title
-        $props.SortTitle = $movie.sorttitle
-        $props.Plot = (-not([string]::IsNullOrEmpty($movie.plot)))
-        $props.Director = ($movie.director.Count -gt 0)
-        $props.Actors = ($movie.actor.Count -gt 0)
+      $props.Json = (Test-Path "$($_.FullName)\*.json")
+      $props.VideoCodec = $videoStream["codec_name"]
+      $props.Height = $videoStream["height"]
+      $props.Width = $videoStream["width"]
+      $props.AudioTracks = $audioStreams.Count
+      foreach ($track in $audioStreams) {
+        $props.AudioCodecs += $track["codec_name"]
       }
-      if ([bool]($fileInfo.format.PSObject.Properties.Name -match "tags")) {
-        $props.Container = $fileInfo.format.tags.encoder
+      $props.InternalChapters = ($fileInfo.ContainsKey("chapters") -and $fileinfo["chapters"].Count -gt 0)
+      $props.ExternalChapters = @(Get-ChildItem "$($_.FullName)\Chapters" -Exclude ".ignore" -ErrorAction SilentlyContinue).Count -gt 0
+      if ($props.Json) {
+        $jsonPath = (Resolve-Path "$($_.FullName)\*.json").ProviderPath
+        $movie = Get-Content $jsonPath -Raw | ConvertFrom-Json -AsHashtable
+        $props.Title = $movie["title"]
+        $props.SortTitle = $movie["sorttitle"]
+        $props.Plot = (-not([string]::IsNullOrEmpty($movie["overview"])))
+        $props.Director = (@($movie["people"] | Where-Object {$_["type"] -eq "Director"}).Count -gt 0)
+        $props.Actors = (@($movie["people"] | Where-Object {$_["type"] -eq "Actor"}).Count -gt 0)
+        $props.Rating = $movie["parentalrating"]
+      }
+      if ($fileInfo["format"].ContainsKey("tags")) {
+        $props.Container = $fileInfo["format"]["tags"]["encoder"]
       }
       Write-Output (New-Object PSObject -Property $props)
     }
