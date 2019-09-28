fb2714d7c757bb7fe133ad5340b0f376c673e536
Henry Buckle <h.buckle@outlook.com>
2019-09-28 18:42:34 +0100

    support mkv

--- Shared/Export-BRSubs.ps1
+++ Shared/Export-BRSubs.ps1
@@ -2,30 +2,51 @@ function Export-BRSubs {
   [CmdletBinding()]
   param (
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
-    [string]$PathToPlaylist,
+    [string]$InputFile,
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
     [string]$OutputFolder,
     [String]$Language = "English"
   )
-  $playlist = Get-Eac3toPlaylist -PathToPlaylist $PathToPlaylist
-  $subs = @()
-  $subs += $playlist.Tracks | Where-Object { $_.Contains("Subtitle (PGS), $Language") }
-  $eac3toargs = @(
-    $playlist.Path,
-    "1)"
-  )
-  foreach ($sub in $subs) {
-    $trackid = $sub.Split(":")[0]
-    if (-not(Test-Path "$OutputFolder\$trackid.sup")) {
-      $eac3toargs += "${trackid}:"
-      $eac3toargs += "$OutputFolder\$trackid.sup"
+  $item = Get-Item $InputFile
+  switch ($item.Extension) {
+    ".mpls" {
+      $playlist = Get-Eac3toPlaylist -PathToPlaylist $InputFile
+      $subs = @()
+      $subs += $playlist.Tracks | Where-Object { $_.Contains("Subtitle (PGS), $Language") }
+      $eac3toargs = @(
+        $playlist.Path,
+        "1)"
+      )
+      foreach ($sub in $subs) {
+        $trackid = $sub.Split(":")[0]
+        if (-not(Test-Path "$OutputFolder\$trackid.sup")) {
+          $eac3toargs += "${trackid}:"
+          $eac3toargs += "$OutputFolder\$trackid.sup"
+        }
+      }
+      & eac3to $eac3toargs
+      foreach ($sub in $subs) {
+        $trackid = $sub.Split(":")[0]
+        if (-not(Test-Path "$OutputFolder\${trackid}_forced.sup")) {
+          Export-ForcedSubs -InputFile "$OutputFolder\$trackid.sup" -OutputFile "$OutputFolder\${trackid}_forced.sup"
+        }
+      }
     }
-  }
-  & eac3to $eac3toargs
-  foreach ($sub in $subs) {
-    $trackid = $sub.Split(":")[0]
-    if (-not(Test-Path "$OutputFolder\${trackid}_forced.sup")) {
-      Export-ForcedSubs -InputFile "$OutputFolder\$trackid.sup" -OutputFile "$OutputFolder\${trackid}_forced.sup"
+    ".mkv" {
+      $info = & mkvextract -J $InputFile | ConvertFrom-Json -AsHashtable
+      $subs = @()
+      $subs += $info["tracks"] | Where-Object codec -eq "HDMV PGS"
+      foreach ($sub in $subs) {
+        $output = "$OutputFolder\$($item.BaseName)-$($sub['id']).sup"
+        $output_forced = "$OutputFolder\$($item.BaseName)-$($sub['id'])_forced.sup"
+        & mkvextract tracks $InputFile $($sub["id"]):${output}
+        if (-not(Test-Path $output_forced)) {
+          Export-ForcedSubs -InputFile $output -OutputFile $output_forced
+        }
+      }
+    }
+    Default {
+      Write-Warning "Unsupported file type $($item.Extension)"
     }
   }
 }
