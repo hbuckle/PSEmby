ebeafe496488c922a67d3e48c023da8acd9c3bdc
Henry Buckle <h.buckle@outlook.com>
2019-12-14 14:18:16 +0000

    support episode groups

--- TV/Get-TvSeason.ps1
+++ TV/Get-TvSeason.ps1
@@ -1,11 +1,35 @@
 function Get-TvSeason {
   [CmdletBinding()]
   param (
-    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+    [Parameter(Mandatory = $true, ParameterSetName = "Id")]
+    [ValidateNotNullOrEmpty()]
     [int]$ShowId,
-    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
-    [int]$SeasonNumber
+
+    [Parameter(Mandatory = $true, ParameterSetName = "Id")]
+    [ValidateNotNullOrEmpty()]
+    [int]$SeasonNumber,
+
+    [Parameter(Mandatory = $true, ParameterSetName = "EpisodeGroup")]
+    [ValidateNotNullOrEmpty()]
+    [string]$EpisodeGroupId,
+
+    [Parameter(Mandatory = $true, ParameterSetName = "EpisodeGroup")]
+    [ValidateNotNullOrEmpty()]
+    [string]$SeasonId
   )
   $client = [tmdbclient]::new($Script:tmdb_api_key)
-  return $client.gettvseason($ShowId, $SeasonNumber)
+  switch ($PSCmdlet.ParameterSetName) {
+    "Id" {
+      return $client.gettvseason($ShowId, $SeasonNumber)
+    }
+    "EpisodeGroup" {
+      $group = $client.gettvepisodegroup($EpisodeGroupId)
+      $season = $group["groups"] | Where-Object id -eq $SeasonId
+      if ($null -eq $season) {
+        throw "Episode group $EpisodeGroupId/group/$SeasonId not found"
+      }
+      return $season
+    }
+    Default { }
+  }
 }
