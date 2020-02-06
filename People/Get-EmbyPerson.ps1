a751757a385d6de87f1e506c33eb6f973c6b2acf
Henry Buckle <h.buckle@outlook.com>
2020-02-06 09:00:49 +0000

    update person

--- People/Get-EmbyPerson.ps1
+++ People/Get-EmbyPerson.ps1
@@ -1,16 +1,40 @@
 function Get-EmbyPerson {
-  [CmdletBinding()]
+  [CmdletBinding(DefaultParameterSetName = "List")]
   param (
     [ValidateNotNullOrEmpty()]
+    [Parameter(ParameterSetName = "Name")]
+    [Parameter(ParameterSetName = "Id")]
+    [Parameter(ParameterSetName = "List")]
     [string]$Server = "https://emby.crucible.org.uk",
+
     [ValidateNotNullOrEmpty()]
+    [Parameter(ParameterSetName = "Name")]
+    [Parameter(ParameterSetName = "Id")]
+    [Parameter(ParameterSetName = "List")]
     [string]$ApiKey = $Script:emby_api_key,
-    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+
+    [Parameter(ParameterSetName = "Name")]
+    [string]$Name,
+
+    [Parameter(ParameterSetName = "Id")]
     [string]$Id
   )
   $builder = [System.UriBuilder]::new($Server)
-  $builder.Path = "Users/${ApiKey}/Items/${Id}"
-  $builder.Query = "api_key=$ApiKey"
-  Invoke-RestMethod $builder.ToString() -Method "Get" -ContentType "application/json" |
-  ConvertTo-Json | ConvertFrom-Json -AsHashtable
+  if ($PSCmdlet.ParameterSetName -eq "Name") {
+    $builder.Path = "emby/Search/Hints"
+    $builder.Query = "api_key=${ApiKey}&SearchTerm=${Name}&IncludePeople=true&IncludeItemTypes=Person"
+    $response = Invoke-RestMethod $builder.ToString() -Method "Get" -ContentType "application/json"
+    $response.SearchHints | Write-Output
+  }
+  elseif ($PSCmdlet.ParameterSetName -eq "Id") {
+    $builder.Path = "Users/${ApiKey}/Items/${Id}"
+    $builder.Query = "api_key=${ApiKey}"
+    Invoke-RestMethod $builder.ToString() -Method "Get" -ContentType "application/json"
+  }
+  else {
+    $builder.Path = "Persons"
+    $builder.Query = "api_key=${ApiKey}"
+    $response = Invoke-RestMethod $builder.ToString() -Method "Get" -ContentType "application/json"
+    $response.Items | Write-Output
+  }
 }
