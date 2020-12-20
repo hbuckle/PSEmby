3b418450f8635eb4ed0db38e34ee957495946c58
Henry Buckle <h.buckle@outlook.com>
2020-12-20 13:31:36 +0000

    use search api

--- Film/Get-FilmDescription.ps1
+++ Film/Get-FilmDescription.ps1
@@ -4,42 +4,32 @@ function Get-FilmDescription {
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
     [string]$Title
   )
-  $filmuri = "https://www.radiotimes.com"
-  $searchterm = [System.Uri]::EscapeUriString("site:${filmuri}/film `"${Title}`"")
-  $searchuri = "https://api.cognitive.microsoft.com/bing/v7.0/search?q=${searchterm}"
-  $headers = @{"Ocp-Apim-Subscription-Key" = $Script:bing_api_key }
+  $searchterm = [System.Uri]::EscapeUriString($Title)
+  $listings = Invoke-RestMethod "https://search.api.immediate.co.uk/v4/search?sitekey=radiotimes&search=${searchterm}&tab=programmes-films"
+  $films = $listings.data.results | Where-Object {
+    $_.categories.name -eq "Film"
+  }
+  $film = Select-ItemFromList -List $films -Properties @("title", "description")
+  if ($null -eq $film) {
+    return New-Object -TypeName "PSObject" -Property @{ title = ""; review = "" }
+  }
+  $filmuri = "https://www.radiotimes.com$($film.url)"
+  $parser = [AngleSharp.Html.Parser.HtmlParser]::new()
+  $reviewpage = Invoke-RestMethod $filmuri
+  $reviewdocument = $parser.ParseDocument($reviewpage)
   $search = @()
-  try {
-    $parser = [AngleSharp.Parser.Html.HtmlParser]::new()
-    $searchresult = Invoke-RestMethod $searchuri -Headers $headers
-    $links = @()
-    if ($null -ne (Get-Member -InputObject $searchresult -Name "webPages")) {
-      foreach ($page in $searchresult.webPages.value) {
-        $links += $page.url
-      }
-    }
-    foreach ($link in $links) {
-      if ($link -eq "https://www.radiotimes.com/film/") {
-        continue
-      }
-      $reviewpage = Invoke-RestMethod $link
-      $reviewdocument = $parser.Parse($reviewpage)
-      $object = @{ }
-      $object["title"] = $reviewdocument.GetElementsByClassName("programme-header__heading  js-programme-page-header").TextContent.Trim()
-      foreach ($review in $reviewdocument.GetElementsByClassName("episode-extra__copy")) {
-        if (@($review.Attributes.Where( { $_.Value -eq "reviewBody" })).Count -gt 0) {
-          $object["review"] = $review.TextContent.Trim()
-          $search += New-Object -TypeName "PSObject" -Property $object
-        }
-      }
-    }
-    $selected = Select-ItemFromList -List $search -Properties @("title", "review")
-    if ($null -eq $selected) {
-      return New-Object -TypeName "PSObject" -Property @{title = ""; review = "" }
-    }
-    else {
-      return $selected
-    }
+  $object = @{ review = "" }
+  $object["title"] = $reviewdocument.GetElementsByClassName("programme-header__heading  js-programme-page-header").TextContent.Trim()
+  $object["review"] = $reviewdocument.GetElementsByClassName("episode-extra__copy") |
+    Where-Object { $_.Attributes.Value -eq "reviewBody" } |
+    Select-Object -ExpandProperty TextContent | ForEach-Object { $_.Trim() }
+  $search += New-Object -TypeName "PSObject" -Property $object
+  $selected = Select-ItemFromList -List $search -Properties @("title", "review")
+  if ($null -eq $selected) {
+    return New-Object -TypeName "PSObject" -Property @{title = ""; review = "" }
+  }
+  else {
+    return $selected
   }
   catch {
     Write-Warning $_.Exception.Message
